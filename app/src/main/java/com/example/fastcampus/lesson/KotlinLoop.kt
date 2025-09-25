fb4b15209462983e4a20a6e24bcfbc0c477eaa3d
Raka Pradana <raka.pradana1103@gmail.com>
2025-09-25 09:59:23 +0700

    materi: loop 2 & study case

--- app/src/main/java/com/example/fastcampus/lesson/KotlinLoop.kt
+++ app/src/main/java/com/example/fastcampus/lesson/KotlinLoop.kt
@@ -1,8 +1,44 @@
 package com.example.fastcampus.lesson
 
-fun main() {
+fun forLoop() {
     val fruits = arrayOf("Apple", "Grape", "Banana", "Kiwi", "Manggo")
     for (i in fruits) {
         println(i)
     }
+    for (i in 1..10) {
+        println(i)
+    }
+}
+
+fun whileLoop() {
+    var start = 1
+    val end = 5
+
+    while (start <= end) {
+        if (start % 2 == 0) {
+            println("$start angka genap")
+        } else {
+            println("$start angka ganjil")
+        }
+        start++
+    }
+}
+
+fun doWhileLoop() {
+    var start = 6
+    val end = 5
+    do {
+        if (start % 2 == 0) {
+            println("$start angka genap")
+        } else {
+            println("$start angka ganjil")
+        }
+        start++
+    } while (start <= end)
+}
+
+fun main() {
+    forLoop()
+//    whileLoop()
+//    doWhileLoop()
 }
--- app/src/main/java/com/example/fastcampus/lesson/studycase/KotlinStudyCase.kt
+++ app/src/main/java/com/example/fastcampus/lesson/studycase/KotlinStudyCase.kt
@@ -1,1 +1,26 @@
 package com.example.fastcampus.lesson.studycase
+
+data class PlayerInfo(
+    val name: String,
+    var level: Int,
+)
+
+fun gameplay(
+    name: String,
+    level: Int,
+) {
+    var player = PlayerInfo(name, level)
+    if (player.level >= 10) {
+        println("Feature unlock")
+    } else {
+        println("Level is too low! Want some exp?")
+        for (exp in player.level..10) {
+            println("leveling up $exp")
+            if (exp == 10) println("Feature unlock")
+        }
+    }
+}
+
+fun main() {
+    gameplay("Player 1", 11)
+}
